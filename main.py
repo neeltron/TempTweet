a9737810e98bc8b09e88aadf3c9f22b8010dc572
Neel Adwani <nprosofficial@gmail.com>
2021-06-30 14:20:52 +0000

    Added Weather API

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import os
 import requests
 
 def fetch_weather_data(location):
-  return 0
+  api_key = os.environ['weather_api']
+  base_url = "http://api.openweathermap.org/data/2.5/weather?"
+  city_name = location
+  complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+  response = requests.get(complete_url)
+  x = response.json()
+  if x["cod"] != "404":
+    y = x["main"]
+    current_temperature = y["temp"] - 273.15
+    return current_temperature
+  else:
+      print("Location out of reach")
+
 
 
 city = input("Enter your City: ")
